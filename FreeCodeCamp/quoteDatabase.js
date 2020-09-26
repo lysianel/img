c8622c0eb899cc444e99e296e7735118f5028459
lysianel <65735946+lysianel@users.noreply.github.com>
2020-09-26 23:00:15 +0200

    Upload Random Quote Machine database

---
+++ FreeCodeCamp/quoteDatabase.js
@@ -0,0 +1,46 @@
+const quoteDatabase = [
+	{
+		quote: "Be yourself; everyone else is already taken.",
+		author:"Oscar Wilde"
+	},
+	{
+		quote:"So many books, so little time.",
+		author:"Frank Zappa"
+	},
+	{
+		quote:"Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
+		author:"Albert Einstein"
+	},
+
+	{
+		quote:"Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.",
+		author:"Bernard M. Baruch"
+	},
+
+	{
+		quote:"A room without books is like a body without a soul.",
+		author:"Marcus Tullius Cicero"
+	},
+
+	{
+		quote:"You've gotta dance like there's nobody watching, Love like you'll never be hurt, Sing like there's nobody listening, And live like it's heaven on earth.",
+		author:"William W. Purkey"
+	},
+
+	{
+		quote:"You only live once, but if you do it right, once is enough.",
+		author:"Mae West"
+	},
+	{
+		quote:"Be the change that you wish to see in the world.",
+		author:"Mahatma Gandhi"
+	},
+	{
+		quote:"In three words I can sum up everything I've learned about life: it goes on.",
+		author:"Robert Frost"
+	},
+	{
+		quote:"If you want to know what a man's like, take a good look at how he treats his inferiors, not his equals.",
+		author:"J.K. Rowling"
+	}
+];
